c4b566dff74c575361be9044bea74fb45b021b77
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2023-05-15 15:16:59 -0300

    Camada de serviço, registro de componente

--- src/main/java/com/luisgmr/springcourse/resources/UserResource.java
+++ src/main/java/com/luisgmr/springcourse/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.luisgmr.springcourse.resources;
 
 import com.luisgmr.springcourse.entities.User;
+import com.luisgmr.springcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Luis", "luis@gmail.com", "988443322", "12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
