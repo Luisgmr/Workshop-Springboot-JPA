c6b09e4bfe1802e3aabbfe88df55c45f472aec9c
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2023-05-15 14:28:19 -0300

    Commit inicial

---
+++ src/main/java/com/luisgmr/springcourse/SpringcourseApplication.java
@@ -0,0 +1,13 @@
+package com.luisgmr.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisgmr/springcourse/SpringcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgmr.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
